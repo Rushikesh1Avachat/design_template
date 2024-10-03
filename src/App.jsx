a48cdc7dfecbb9b3aaa02c30805a043739f90aad
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2024-10-03 14:10:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
-import CarList from "./components/CarList/Carlist";
+import CarList from "./components/CarList/CarList";
 import AppStoreBanner from "./components/AppStoreBanner/AppStoreBanner";
 import Contact from "./components/Contact/Contact";
 import Testimonial from "./components/Testimonial/Testimonial";
